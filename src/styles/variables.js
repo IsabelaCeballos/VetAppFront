856ea8ffee9722863ace5d4af3625a82ba77f6ff
Daniela Jurado Blandón <daniijuradob@gmail.com>
2022-10-09 14:55:43 -0500

    assets files

---
+++ src/styles/variables.js
@@ -0,0 +1,17 @@
+const COLORS = { 
+    rosa: "#F1517F", 
+    morado: "#6C70C9",
+    celeste: "#C4D3DA",
+    negro: "#222222",
+    gris: "#4B4B4B",
+    rojo: "#E86166"
+}
+  
+const TYPOGRAPIC = {
+    nunito: "Nunito, sans-serif"
+}
+  
+export default {
+    COLORS,
+    TYPOGRAPIC
+};
